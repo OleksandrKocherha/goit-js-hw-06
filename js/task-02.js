7b4e28ef7ff54ad9202eab9805638eb9c257deb4
OleksandrKocherha <98402583+OleksandrKocherha@users.noreply.github.com>
2022-11-14 23:36:37 +0200

    /

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,10 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const list = document.querySelector("#ingredients");
+ingredients.forEach((item) => {
+  const listItem = document.createElement("li")
+  listItem.textContent = item;
+  list.append(listItem)
+})
